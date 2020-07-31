68dbc315fac18dff3d28ab28dc516596b70c6f63
Rawad El Kontar <rawad.elkontar@nrel.gov>
2020-07-31 00:55:25 -0600

    updated measure dependency

--- lib/measures/default_feature_reports/measure.rb
+++ lib/measures/default_feature_reports/measure.rb
@@ -28,7 +28,7 @@
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 # *********************************************************************************
 
-require 'urbanopt/scenario/default_reports/feature_report'
+require_relative '../../default_reports'
 require 'csv'
 require 'benchmark'
 require 'logger'
